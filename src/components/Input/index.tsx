e625cc76dac1ad06aa60a0f7525c1a615373f26b
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-07-15 09:36:22 -0300

    started input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { TextInputProps } from 'react-native';
 
 import { Container } from './styles';
+
+interface InputProps extends TextInputProps {
+
+}
 
 const Input: React.FC = () => <Container />;
 
