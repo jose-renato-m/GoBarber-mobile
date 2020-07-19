3d180509a61187fbedfd6252c4a8af1e609e39fd
José Renato Montagnana Leite Perez <jrenato78@gmail.com>
2020-07-19 16:15:13 -0300

    :sparkles: user experience for inputs finalized

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -5,6 +5,7 @@ import {
   ScrollView,
   KeyboardAvoidingView,
   Platform,
+  TextInput,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
@@ -21,6 +22,9 @@ import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
 const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
+
+  const emailInputRef = useRef<TextInput>(null);
+  const passwordInputRef = useRef<TextInput>(null);
 
   return (
     <>
@@ -52,9 +56,13 @@ const SignUp: React.FC = () => {
                 icon="user"
                 placeholder="Nome"
                 returnKeyType="next"
+                onSubmitEditing={() => {
+                  emailInputRef.current?.focus();
+                }}
               />
 
               <Input
+                ref={emailInputRef}
                 keyboardType="email-address"
                 autoCorrect={false}
                 autoCapitalize="none"
@@ -62,9 +70,13 @@ const SignUp: React.FC = () => {
                 icon="mail"
                 placeholder="E-mail"
                 returnKeyType="next"
+                onSubmitEditing={() => {
+                  passwordInputRef.current?.focus();
+                }}
               />
 
               <Input
+                ref={passwordInputRef}
                 secureTextEntry
                 name="password"
                 icon="lock"
